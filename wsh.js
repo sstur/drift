4c1228ffba12b5cb5a29b60057c339e6907bd205
Simon Sturmer <sstur@me.com>
2012-08-19 23:31:30 +0700

    remove node-wsh; update package

--- wsh.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var WSHServer = require('./node-wsh');
-
-var server = http.createServer(WSHServer.requestHandler);
-server.listen(8080, function() {
-  console.log('Server running at http://localhost:8080/');
-  if (WSHServer.init) WSHServer.init();
-});
