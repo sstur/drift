d0764907afbb8a960d7f5332fd07afd4d0a18101
Simon Sturmer <sstur@me.com>
2012-08-23 12:03:26 +0700

    add minification (-m) option to build

--- build-asp.js
+++ build-asp.js
@@ -5,10 +5,31 @@
   var join = require('path').join;
   var Buffer = require('buffer').Buffer;
 
+  try {
+    var uglifyjs = require('uglify-js');
+  } catch(e) {}
+
+  function uglify(code) {
+    //quick hack to remove strict mode declarations
+    code = code.replace(/^\s*("|')use strict\1;?\s*$/gm, '');
+    var parser = uglifyjs.parser;
+    var processor = uglifyjs.uglify;
+    var ast = parser.parse(code);
+    ast = processor.ast_lift_variables(ast);
+    ast = processor.ast_mangle(ast, {toplevel: true});
+    ast = processor.ast_squeeze(ast);
+    return processor.gen_code(ast, {beautify: true, indent_level: 2, ascii_only: true, inline_script: true});
+  }
+
   var REG_NL = /\r\n|\r|\n/g;
 
   var basePath = __dirname
     , buildPath = join(__dirname, 'app/build');
+
+  var opts = process.argv.slice(2).reduce(function(opts, el) {
+    return (opts[el.replace(/^-+/, '')] = 1) && opts;
+  }, {});
+
   var sourceFiles = []
     , sourceLines = [];
 
@@ -42,8 +63,6 @@
     });
   };
 
-  sourceLines.push('<%@LANGUAGE="JAVASCRIPT" CODEPAGE="65001"%>');
-  sourceLines.push('<script runat="server" language="javascript">');
   sourceLines.push('(function(global) {');
 
   //load framework core (instantiates `app` and `define`)
@@ -67,7 +86,21 @@
   loadFile('app/system/adapters', 'asp.js');
 
   sourceLines.push('})({Request: Request, Response: Response, Server: Server, Application: Application})');
+
+  if (opts.m) {
+    if (!uglifyjs) {
+      console.err('Cannot find module uglify-js.');
+      process.exit();
+    }
+    sourceLines = [uglify(sourceLines.join('\n'))];
+  } else
+  if (opts.e) {
+    //toso: add source-line mapping for error handling
+  }
+
+  sourceLines.unshift('<%@LANGUAGE="JAVASCRIPT" CODEPAGE="65001"%>', '<script runat="server" language="javascript">');
   sourceLines.push('</script>');
+
 
   //construct buffer including byte-order-mark and source
   var bom = new Buffer('EFBBBF', 'hex'), source = sourceLines.join('\r\n'), sourceLength = Buffer.byteLength(source);
