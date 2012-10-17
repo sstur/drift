f90ad4944382835a90961e6a435ad4a6cb33e064
Simon Sturmer <sstur@me.com>
2012-10-17 21:25:33 +0700

    remove app specific stuff

--- app/config/config.js
+++ app/config/config.js
@@ -2,8 +2,6 @@ define('config', function(require, exports, module) {
   "use strict";
 
   module.exports = {
-    //if we are intentionally embedding the routes in the query
-    virtual_url: true,
     //variables passed to template engine
     template_defaults: {
       reseller: {name: 'Domain Company', domain: 'company.xappr.com'}
