7967fb6b68946be083dac24bff3771671e28326e
Simon Sturmer <sstur@me.com>
2013-06-09 06:10:24 +0000

    add mysql and model

---
+++ app/system/adapters/activex/mysql.js
@@ -0,0 +1,350 @@
+/*global app, define, Buffer, Enumerator, executeSqlAndReturnNumRowsAffected */
+define('mysql', function(require, exports) {
+  "use strict";
+
+  var util = require('util');
+
+  var RE_NONASCII = /[\x00-\x1f\x7f-\xff\u0100-\uffff]+/g;
+  var RE_UNICODE = /[\x7f-\xff\u0100-\uffff]+/g;
+  var namedConnections = app.cfg('mysql/connections') || {};
+  var connections = {};
+
+  function Connection(name, conn_str) {
+    this._cstr = conn_str;
+    var conn;
+    if (name in connections) {
+      conn = connections[name];
+    } else {
+      conn = new ActiveXObject("ADODB.Connection");
+    }
+    if (conn.state == 0) {
+      try {
+        conn.open(this._cstr);
+      } catch(e) {
+        var message = cleanError(e.message);
+        //todo: "Table 'db.tbl' doesn't exist"
+        if (message.match(/^Could not find table/i)) {
+          //todo: create table
+          this.isNew = true;
+          conn.open(this._cstr);
+        } else {
+          throw e;
+        }
+      }
+    }
+    this._conn = conn;
+  }
+
+  function Query(conn, str, params) {
+    this.str = str;
+    this.conn = conn;
+    this.params = params;
+  }
+
+  util.extend(Query.prototype, {
+    getSQL: function() {
+      return this.sql || (this.sql = buildSQL(this.str, this.params));
+    },
+    each: function(func) {
+      var sql = this.getSQL();
+      util.log(3, sql, 'mysql');
+      try {
+        var rs = this.conn.execute(sql);
+      } catch (e) {
+        throw new Error('SQL Statement Could not be executed. ' + cleanError(e.message) + '\r\n' + sql);
+      }
+      var abort = false, i = 0;
+      if (rs.state) {
+        while (!rs.eof && !abort) {
+          var rec = {};
+          enumerate(rs.fields, function(i, field) {
+            rec[field.name] = fromADO(field.value);
+          });
+          abort = ( func(rec, i++) === false );
+          rs.movenext();
+        }
+        rs.close();
+      }
+      return this;
+    },
+    getOne: function() {
+      var rec;
+      this.each(function(r) {
+        rec = r;
+        return false;
+      });
+      return rec;
+    },
+    getAll: function() {
+      var arr = [];
+      this.each(function(r) {
+        arr.push(r);
+      });
+      return arr;
+    }
+  });
+
+  util.extend(Connection.prototype, {
+    query: function(str, params, func) {
+      if (arguments.length == 1) {
+        params = [];
+      }
+      if (vartype(params) == 'function') {
+        func = params;
+        params = [];
+      }
+      var query = new Query(this._conn, str, params);
+      //Apply passed in function
+      if (func) query.each(func);
+      return query;
+    },
+    insert: function(table, data, returnAffected) {
+      var sql = [], names = [], values = [];
+      Object.keys(data).forEach(function(key) {
+        sql.push('?');
+        names.push('`' + key + '`');
+        values.push(data[key]);
+      });
+      sql = 'INSERT INTO `' + table + '` (' + names.join(', ') + ') VALUES (' + sql.join(', ') + ')';
+      return this.exec(sql, values, returnAffected);
+    },
+    exec: function(str, params, returnAffected) {
+      var i, conn = this._conn, sql = buildSQL(str, params);
+      util.log(3, sql, 'mysql');
+      try {
+        if (returnAffected) {
+          i = executeSqlAndReturnNumRowsAffected(conn, sql);
+        } else {
+          conn.execute(sql, i, 128);
+        }
+      } catch (e) {
+        throw new Error('SQL Statement Could not be executed. ' + cleanError(e.message) + '\r\n' + sql);
+      }
+      if (returnAffected) {
+        if (String(sql).match(/^INSERT/i)) {
+          sql = 'SELECT LAST_INSERT_ID() AS `val`';
+        } else {
+          return Number.parseInt(i);
+        }
+        this.query(sql, function(rec) {
+          i = rec.val
+        });
+        return Number.parseInt(i);
+      }
+    },
+    close: function() {
+      var conn = this._conn;
+      if (conn.state != 0) conn.close();
+    }
+  });
+
+  var REG_SQL_ENTITIES = /('(''|[^'])*'|\[(\\.|[^\]])*\]|\$\d+|\?|[A-Z_]+\(\))/gim;
+
+  function buildSQL(str, params) {
+    var sql = String(str), arr = [0].concat(params);
+    //escape array values
+    forEach(arr, function(i, val) {
+      arr[i] = toSQLVal(val);
+    });
+    var i = 0, j = arr.length, val, date = new Date();
+    //sub out quoted literals and ? placeholders
+    sql = sql.replace(REG_SQL_ENTITIES, function(s) {
+      var c = s.substr(0, 1);
+      if (c == "?") {
+        val = arr[++i];
+        s = (val == 'NULL') ? val : '$' + i;
+      } else
+      if (c == "$") {
+        val = arr[Number.parseInt(s.substr(1))];
+        if (val == 'NULL') {
+          s = val;
+        }
+      } else
+      if (c == "'") {
+        arr[j] = s;
+        s = '$' + (j++);
+      }
+      return s;
+    });
+    //misc sql transforms
+    sql = sql.replace(/\bNOW\(\)/ig, toSQLVal(date));
+    //replace special CAST values
+    sql = sql.replace(/(\w+)\(\$(\d)\)/, function(s, n, i) {
+      var val = arr[Number.parseInt(i)];
+      var sql = s.replace('$' + i, val);
+      if (n == 'CAST_DATE') {
+        sql = toSQLVal(parseDate(val.slice(1, -1)));
+      }
+      sql = sql.replace(/CAST_HEX\('((?:[0-9a-f]{2})+)'\)/ig, '0x$1');
+      return sql;
+    });
+    //re-insert subbed-out entities
+    sql = sql.replace(/\$(\d+)/g, function(s, i) {
+      i = Number.parseInt(i);
+      return arr[i];
+    });
+    return sql;
+  }
+
+  function toSQLVal(val) {
+    if (val == null) {
+      return 'NULL';
+    }
+    var type = typeof val;
+    if (type == 'boolean') {
+      return (val) ? 'true' : 'false';
+    }
+    if (type == 'number') {
+      //the big number is 2^64-1; the max number for LIMIT
+      return (isNaN(val)) ? 'NULL' : (isFinite(val) ? val.toString() : (val < 0 ? '-' : '') + '18446744073709551615');
+    }
+    if (val instanceof Date) {
+      return "'" + formatDate(val) + "'";
+    }
+    if (Buffer.isBuffer(val)) {
+      return "x'" + val.toString('hex') + "'";
+    }
+    if (Array.isArray(val) || type == 'object') {
+      val = (val.toString) ? val.toString() : Object.prototype.toString.call(val);
+    }
+    return escSqlString(val);
+  }
+
+  function formatDate(date) {
+    //Dates are stored in DB as UTC
+    //format is: {yyyy}-{mm}-{dd} {HH}:{nn}:{ss}
+    return date.getUTCFullYear() + '-' +
+      pad(date.getUTCMonth() + 1) + '-' +
+      pad(date.getUTCDate()) + ' ' +
+      pad(date.getUTCHours()) + ':' +
+      pad(date.getUTCMinutes()) + ':' +
+      pad(date.getUTCSeconds());
+  }
+
+  function pad(n) {
+    return ('0' + n).slice(-2);
+  }
+
+  var REG_DATE_1 = /^(\d{4})-(\d{2})-(\d{2})\s*T?([\d:]+)(\.\d+)?($|[Z\s+-].*)$/i;
+  var REG_DATE_2 = /\b(\d{4})-(\d{1,2})-(\d{1,2})\b/;
+
+  function parseDate(input, /**String=*/ def) {
+    if (input instanceof Date) {
+      return new Date(input);
+    }
+    var str = String(input);
+    //ISO 8601 / JSON-style date: "2008-12-13T16:08:32Z"
+    str = str.replace(REG_DATE_1, '$1/$2/$3 $4$6');
+    //YYYY-MM-DD
+    str = str.replace(REG_DATE_2, '$1/$2/$3');
+    var i = Date.parse(str);
+    if (isFinite(i)) {
+      return new Date(i);
+    }
+    if (arguments.length > 1) {
+      return def;
+    }
+    throw new Error('Not a date: ' + input);
+  }
+
+  function parseUTCDate(input, /**String=*/ def) {
+    var d = parseDate(input, def);
+    return new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.valueOf() % 1000));
+  }
+
+  function escSqlString(val) {
+    val = val.replace(/[\0\n\r\b\t\\'\x1A]/g, function(s) {
+      switch(s) {
+        case "\0": return "\\0";
+        case "\n": return "\\n";
+        case "\r": return "\\r";
+        case "\b": return "\\b";
+        case "\t": return "\\t";
+        case "\x1a": return "\\Z";
+        default: return "\\" + s;
+      }
+    });
+    if (!RE_NONASCII.test(val)) {
+      return "'" + val + "'";
+    }
+    //do some funky encoding for unicode
+    var pieces = [], lastPos = 0;
+    val.replace(RE_NONASCII, function(ch, pos) {
+      if (pos > lastPos) {
+        pieces.push("'" + val.slice(lastPos, pos) + "'");
+      }
+      pieces.push("0x" + encodeURI(ch).replace(/%/g, ''));
+      lastPos = pos + ch.length;
+    });
+    if (val.length > lastPos) {
+      pieces.push("'" + val.slice(lastPos) + "'");
+    }
+    return "CONCAT(" + pieces.join(',') + ")";
+  }
+
+  //Convert from ADO Data Type
+  function fromADO(val) {
+    var type = typeof val;
+    if (type == 'string' && RE_UNICODE.test(val)) {
+      return tryDecodeMultibyte(val);
+    } else
+    if (type == 'date') {
+      //Dates are stored in DB as UTC
+      return parseUTCDate(val);
+    } else
+    if (type == 'unknown') {
+      return new Buffer(val);
+    }
+    return val;
+  }
+
+  var REG_ESC_UNICODE = /%u([0-9a-f]{4})/ig;
+  var WIN1252 = {"2013": "96", "2014": "97", "2018": "91", "2019": "92", "2020": "86", "2021": "87", "2022": "95",
+    "2026": "85", "2030": "89", "2039": "8b", "2122": "99", "20AC": "80", "201A": "82", "0192": "83", "201E": "84",
+    "02C6": "88", "0160": "8a", "0152": "8c", "017D": "8e", "201C": "93", "201D": "94", "02DC": "98", "0161": "9a",
+    "203A": "9b", "0153": "9c", "017E": "9e", "0178": "9f"};
+
+  function tryDecodeMultibyte(str) {
+    var enc = escape(str).replace(REG_ESC_UNICODE, function(_, hex) {
+      var code = WIN1252[hex.toUpperCase()];
+      return code ? '%' + code : _;
+    });
+    try {
+      return decodeURIComponent(enc);
+    } catch(e) {
+      return str;
+    }
+  }
+
+  function enumerate(col, fn) {
+    var i = 0;
+    new Enumerator(col);
+    for(var e = new Enumerator(col); !e.atEnd(); e.moveNext()) {
+      if (fn.call(col, i++, e.item()) === false) break;
+    }
+  }
+
+  function cleanError(message) {
+    message = String(message);
+    return message.replace(/^(\[[^\]]+\])+/, '');
+  }
+
+  app.on('end', function() {
+    forEach(connections, function(name, conn) {
+      if (conn.state != 0) conn.close();
+    });
+  });
+
+  exports.open = function(conn_name, initFunc) {
+    var conn_str = namedConnections[conn_name || 'default'];
+    if (!conn_str) {
+      throw new Error('Invalid MySQL Connection');
+    }
+    var conn = new Connection(conn_name, conn_str);
+    if (conn.isNew && initFunc) {
+      initFunc(conn);
+    }
+    return conn;
+  };
+
+});
---
+++ app/system/lib/model.js
@@ -0,0 +1,297 @@
+/*global app, define */
+define('model', function(require, exports) {
+  "use strict";
+  var util = require('util');
+  var mysql = require('mysql');
+
+  var _hasOwnProperty = Object.prototype.hasOwnProperty;
+
+  var COMPARATORS = {
+    $lt: '<',
+    $gt: '>',
+    $eq: '=',
+    $ne: '!=',
+    $in: 'IN',
+    $nin: 'NOT IN'
+  };
+
+  function Model(opts) {
+    if (!(this instanceof Model)) return new Model(opts);
+    this.tableName = opts.tableName || error('Invalid Table Name');
+    var fields = this.fields = opts.fields || {};
+    this.fieldNames = Object.keys(fields);
+    this.idField = opts.idField || getFirstKey(fields) || error('Invalid ID Field');
+    var jsonFields = this.jsonFields = [];
+    forEach(fields, function(name, definition) {
+      if (definition && definition.type == 'json') {
+        jsonFields.push(name);
+      }
+    });
+    this.fieldMap = opts.fieldMap || {};
+    this.reverseFieldMap = invert(opts.fieldMap);
+    this.dbFieldNames = Object.keys(this._mapToDB(fields));
+    this.dbIdField = opts.dbIdField || this.reverseFieldMap[this.idField] || this.idField;
+    this.autoIncrement = opts.autoIncrement;
+    if (opts.classMethods) {
+      util.extend(this, opts.classMethods);
+    }
+    //record instances are of self class
+    function RecordClass(data) {
+      return Record.apply(this, arguments);
+    }
+    util.inherits(RecordClass, Record);
+    if (opts.instanceMethods) {
+      util.extend(RecordClass.prototype, opts.instanceMethods);
+    }
+    RecordClass.prototype._model = this;
+    this.RecordClass = RecordClass;
+  }
+  exports.Model = Model;
+
+  util.extend(Model.prototype, {
+    _mapFromDB: function(obj) {
+      return mapKeys(obj, this.fieldMap);
+    },
+    _mapToDB: function(obj) {
+      return mapKeys(obj, this.reverseFieldMap);
+    },
+    _reviveFields: function(rec) {
+      this.jsonFields.forEach(function(fieldName) {
+        revive(rec, fieldName);
+      });
+    },
+    create: function(rec) {
+      rec = this._mapFromDB(rec);
+      this._reviveFields(rec);
+      return new this.RecordClass(rec);
+    },
+    insert: function(data) {
+      var instance = this.create(data);
+      instance.insert();
+      return instance;
+    },
+    find: function(params, opts) {
+      params = this._mapToDB(params);
+      opts = opts || {};
+      opts.limit = 1;
+      var built = buildSelect(this, params, opts);
+      var db = mysql.open();
+      var rec = db.query(built.sql, built.values).getOne();
+      return rec ? this.create(rec) : null;
+    },
+    //todo: opts.start and opts.limit
+    findAll: function(params, opts) {
+      params = params ? this._mapToDB(params) : {};
+      var built = buildSelect(this, params, opts);
+      var db = mysql.open();
+      var query = db.query(built.sql, built.values);
+      var results = [], self = this;
+      query.each(function(rec) {
+        results.push(self.create(rec));
+      });
+      return results;
+    },
+    count: function(params) {
+      params = this._mapToDB(params);
+      var built = buildCount(this, params);
+      var db = mysql.open();
+      var rec = db.query(built.sql, built.values).getOne();
+      return rec.count;
+    }
+  });
+
+  function Record(data) {
+    util.extend(this, data);
+    if (this.init) this.init();
+  }
+  exports.Record = Record;
+
+  util.extend(Record.prototype, {
+    update: function(data) {
+      var model = this._model;
+      if (data) {
+        //filter data
+        data = filterObject(data, model.fieldNames);
+        //remove id field
+        delete data[model.idField];
+        util.extend(this, data);
+      } else {
+        //get data from this
+        data = filterObject(this, model.fieldNames);
+        delete data[model.idField];
+      }
+      //stringify json fields
+      model.jsonFields.forEach(function(fieldName) {
+        if (fieldName in data) {
+          data[fieldName] = util.stringify(data[fieldName]);
+        }
+      });
+      var built = buildUpdate(this, model._mapToDB(data));
+      var db = mysql.open();
+      db.exec(built.sql, built.values);
+    },
+    insert: function() {
+      var model = this._model;
+      //filter data
+      var data = filterObject(this, model.fieldNames);
+      //remove id field
+      if (model.autoIncrement !== false) {
+        delete data[model.idField];
+      }
+      //stringify json fields
+      model.jsonFields.forEach(function(fieldName) {
+        if (fieldName in data) {
+          data[fieldName] = util.stringify(data[fieldName]);
+        }
+      });
+      var db = mysql.open();
+      this[model.idField] = db.insert(model.tableName, model._mapToDB(data), true);
+    },
+    toJSON: function() {
+      return util.extend({}, this);
+    }
+  });
+
+
+  /*!
+   * Helpers
+   */
+
+  function buildSelect(model, params, opts) {
+    opts = opts || {};
+    var fields = model.fields, dbFieldNames = model.dbFieldNames;
+    //optionally select only certain fields
+    if (opts.fields && opts.fields.length) {
+      fields = filterObject(fields, opts.fields);
+      dbFieldNames = Object.keys(model._mapToDB(fields));
+    }
+    var fieldNames = '`' + dbFieldNames.join('`, `') + '`';
+    var sql = 'SELECT ' + fieldNames + ' FROM `' + model.tableName + '`';
+    var where = buildWhere(params);
+    if (where.terms.length) {
+      sql += ' WHERE ' + where.terms.join(' AND ');
+    }
+    var values = where.values;
+    if (opts.search) {
+      var search = model._mapToDB(opts.search.fields);
+      var searchTerms = [];
+      forEach(search, function(field, text) {
+        values.push(normalizeSearchText(text));
+        searchTerms.push('`' + field + '` LIKE ?');
+      });
+      sql += ' AND (' + searchTerms.join(' OR ') + ')';
+    }
+    if (opts.orderBy && (opts.orderBy in fields)) {
+      var orderBy = model.reverseFieldMap[opts.orderBy] || opts.orderBy;
+      sql += ' ORDER BY `' + orderBy + '`' + (opts.dir ? ' ' + opts.dir.toUpperCase() : '');
+    }
+    if (opts.limit || opts.offset) sql += ' LIMIT ' + (opts.limit || '18446744073709551615'); //2^64-1
+    if (opts.offset) sql += ' OFFSET ' + opts.offset;
+    return {sql: sql, values: values};
+  }
+
+  function buildCount(model, params) {
+    var sql = 'SELECT COUNT(`' + model.dbIdField + '`) AS `count` FROM `' + model.tableName + '`';
+    var where = buildWhere(params);
+    if (where.terms.length) {
+      sql += ' WHERE ' + where.terms.join(' AND ');
+    }
+    return {sql: sql, values: where.values};
+  }
+
+  function buildUpdate(instance, data) {
+    var terms = [];
+    var values = [];
+    forEach(data, function(n, val) {
+      terms.push('`' + n + '` = ?');
+      values.push(val);
+    });
+    var model = instance._model;
+    var sql = 'UPDATE `' + model.tableName + '` SET ' + terms.join(', ') + ' WHERE `' + model.dbIdField + '` = ?';
+    values.push(instance[model.idField]);
+    return {sql: sql, values: values};
+  }
+
+  function buildWhere(params) {
+    var terms = [];
+    var values = [];
+    forEach(params, function(fieldName, term) {
+      term = parseTerm(term);
+      terms.push('`' + fieldName + '` ' + term.sql);
+      values.push.apply(values, term.values);
+    });
+    return {terms: terms, values: values};
+  }
+
+  function isQuery(term) {
+    if (term == null) return false;
+    var isPlainObject = (term.toString === Object.prototype.toString);
+    return isPlainObject && Object.keys(term).join('').match(/^(\$\w+)+$/);
+  }
+
+  function parseTerm(term) {
+    //if term is value (including Date/Buffer) vs a Query
+    if (!isQuery(term)) term = {$eq: term};
+    var key = getFirstKey(term), op = COMPARATORS[key], value = term[key];
+    if (Array.isArray(value)) {
+      var sql = op + ' (' + new Array(value.length + 1).join('?').split('').join(', ') + ')';
+      var values = value;
+    } else {
+      sql = op + ' ?';
+      values = [value];
+    }
+    return {sql: sql, values: values};
+  }
+
+  function error(str) {
+    throw new Error('Model Error; ' + str);
+  }
+
+  function invert(obj) {
+    var result = {};
+    forEach(obj, function(key, val) {
+      result[val] = key;
+    });
+    return result;
+  }
+
+  function mapKeys(obj, map) {
+    var keys = Object.keys(obj), result = {};
+    keys.forEach(function(key) {
+      var value = obj[key];
+      if (key in map) key = map[key];
+      result[key] = value;
+    });
+    return result;
+  }
+
+  function filterObject(obj, keys) {
+    var result = {};
+    keys.forEach(function(key) {
+      if (_hasOwnProperty.call(obj, key)) {
+        result[key] = obj[key];
+      }
+    });
+    return result;
+  }
+
+  function getFirstKey(obj) {
+    for (var key in obj) {
+      if (_hasOwnProperty.call(obj, key)) return key;
+    }
+    return null;
+  }
+
+  function normalizeSearchText(text) {
+    return '%' + text.toLowerCase().replace(/[^\w]+/g, ' ').trim().replace(/\s+/g, '%') + '%';
+  }
+
+  function revive(obj, prop) {
+    var opts = obj[prop];
+    if (typeof opts == 'string') {
+      opts = obj[prop] = util.parse(opts || '{}'); //catch empty string
+    }
+    return opts || (obj[prop] = {}); //catch null
+  }
+
+});
