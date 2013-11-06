5fabddbe338e23564c58a7d82ce66f3bbe7ecb00
Simon Sturmer <sstur@me.com>
2013-11-06 10:17:09 -0800

    add test req/res

---
+++ app/system/adapters/test/request.js
@@ -0,0 +1,48 @@
+/*global app, define */
+define('adapter-request', function(require, exports, module) {
+  "use strict";
+  var qs = require('qs');
+  var util = require('util');
+  var Buffer = require('buffer').Buffer;
+  var BodyParser = require('body-parser');
+
+  var REG_URL = /^([^:\/]+:\/\/)?([^\/]*)(.*)$/;
+
+  function Request(data) {
+    data = data || {};
+    data.url = data.url || '/';
+    data.method = data.method || 'GET';
+    data.headers = data.headers || {};
+    data.remoteAddr = data.remoteAddr || '127.0.0.1';
+    this._data = data;
+  }
+
+  util.extend(Request.prototype, {
+    getMethod: function() {
+      return this._data.method;
+    },
+    getURL: function() {
+      return this._data.url;
+    },
+    getHeaders: function() {
+      return this._data.headers;
+    },
+    getRemoteAddress: function() {
+      return this._data.remoteAddr;
+    },
+    _read: function(bytes) {
+      //todo: read from file-system for testing
+      throw new Error('Could not read ' + bytes + ' bytes from request body');
+    },
+    parseReqBody: function(parent) {
+      var opts = {
+        autoSavePath: ('autoSavePath' in parent) ? parent.autoSavePath : app.cfg('auto_save_uploads')
+      };
+      var parser = new BodyParser(this.getHeaders(), this._read.bind(this), opts);
+      util.propagateEvents(parser, parent, 'file upload-progress');
+      return parser.parse();
+    }
+  });
+
+  module.exports = Request;
+});
---
+++ app/system/adapters/test/response.js
@@ -0,0 +1,39 @@
+/*global app, define */
+define('adapter-response', function(require, exports, module) {
+  "use strict";
+
+  var util = require('util');
+
+  function Response() {
+    this.status = '';
+    this.headers = {};
+    this.body = [];
+  }
+
+  util.extend(Response.prototype, {
+    writeHead: function(statusCode, statusReason, headers) {
+      var self = this;
+      self.status = statusCode + ' ' + statusReason;
+      forEach(headers, function(n, val) {
+        val = (val == null) ? '' : String(val);
+        self.headers[n] = val;
+      });
+      if (app.cfg('debug_open_connections')) {
+        self.headers['X-Open-DB-Connections'] = app.data('debug:open_connections') || 'None';
+      }
+    },
+    write: function(data) {
+      this.body.push(typeof data == 'string' ? data : toString(data));
+    },
+    end: function() {
+      throw null;
+    }
+  });
+
+  var _toString = Object.prototype.toString;
+  function toString(obj) {
+    return (obj == null) ? String(obj) : (typeof obj.toString == 'funtion') ? obj.toString() : _toString.call(obj);
+  }
+
+  module.exports = Response;
+});
