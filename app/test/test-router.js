3e4d7651e5bfd6ea124e2a173553589120fdcee4
Simon Sturmer <sstur@me.com>
2013-11-13 23:25:46 -0800

    route tests

--- app/test/test-router.js
+++ app/test/test-router.js
@@ -35,6 +35,50 @@ app.on('ready', function(require) {
       expect(params).to.eql(['one', 'two']);
       router.route('GET', '/a/s/d', null);
       expect(params).to.eql([null, 's', 'd']);
+    },
+    'optional params': function() {
+      this.setup();
+      var params = null;
+      router.addRoute('/a/:b/:c?', function(b, c) {
+        params = toArray(arguments);
+      });
+      router.route('GET', '/a');
+      expect(params).to.be(null);
+      router.route('GET', '/a/test', 1);
+      expect(params).to.eql([1, 'test', '']);
+      router.route('GET', '/a/b/c', 'req', 'res');
+      expect(params).to.eql(['req', 'res', 'b', 'c']);
+    },
+    'this params': function() {
+      this.setup();
+      router.addRoute('/a/:b/:c?', function(b, c) {
+        expect(this.opts).to.eql({});
+        expect(this.values).to.eql([b, c]);
+        expect(this.params).to.eql({b: b, c: c});
+      });
+      router.route('GET', '/a/b/c');
+    },
+    'route options': function() {
+      this.setup();
+      router.addRoute('/a/:b/:c?', function(b, c) {
+        expect(this.opts).to.eql({noAuth: 1});
+        expect(this.values).to.eql(['1', '2']);
+        expect(this.params).to.eql({b: '1', c: '2'});
+      }, {noAuth: 1});
+      router.route('GET', '/a/1/2');
+    },
+    'route stop': function() {
+      this.setup();
+      var count = 0;
+      router.addRoute('/a', function() {
+        count += 1;
+        this.stop();
+      });
+      router.addRoute('/a', function() {
+        count += 1;
+      });
+      router.route('GET', '/a');
+      expect(count).to.be(1);
     }
   });
 
