8b7521f56ed3bc3987874492174533d9e3f24c5e
Simon Sturmer <sstur@me.com>
2013-11-14 09:34:14 -0800

    update crypto tests

--- app/test/test-crypto.js
+++ app/test/test-crypto.js
@@ -8,63 +8,61 @@ app.on('ready', function(require) {
   var hash, result, key, keyBuffer;
 
   app.addTestSuite('crypto', {
-    'MD5 Test 1': function() {
+    'md5': function() {
       hash = crypto.createHash('md5');
       hash.update('hello', 'utf8');
       result = hash.digest('hex');
       expect(result).to.be('5d41402abc4b2a76b9719d911017c592');
     },
-    'MD5 Test 2': function() {
+    'md5 shorthand': function() {
       result = crypto.hash('md5', 'hello', 'utf8').toString('hex');
       expect(result).to.be('5d41402abc4b2a76b9719d911017c592');
     },
-    'MD5 Test 3': function() {
+    'md5 mixed input encoding': function() {
       hash = crypto.createHash('md5');
       hash.update('test1', 'utf8');
       hash.update('fb0a30e26e83b2ac5b9e29', 'hex');
       result = hash.digest('hex');
       expect(result).to.be('d14b690f60f217b05e9f3206916a0da3');
     },
-    'MD5 Test 4': function() {
-      key = result; //d14b690f60f217b05e9f3206916a0da3
+    'md5 hmac': function() {
+      key = 'd14b690f60f217b05e9f3206916a0da3';
       keyBuffer = new Buffer(key, 'hex');
-
       hash = crypto.createHmac('md5', keyBuffer);
       hash.update('hello', 'utf8');
       result = hash.digest('hex');
       expect(result).to.be('d54d0414c3e8460933cb6f9b082a38ac');
     },
-    'MD5 Test 5': function() {
+    'md5 hmac shorthand': function() {
       result = crypto.hmac('md5', keyBuffer, 'hello', 'utf8').toString('hex');
       expect(result).to.be('d54d0414c3e8460933cb6f9b082a38ac');
     },
-    'SHA256 Test 1': function() {
+    'sha256': function() {
       hash = crypto.createHash('sha256');
       hash.update('hello', 'utf8');
       result = hash.digest('hex');
       expect(result).to.be('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824');
     },
-    'SHA256 Test 2': function() {
+    'sha256 shorthand': function() {
       result = crypto.hash('sha256', 'hello', 'utf8').toString('hex');
       expect(result).to.be('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824');
     },
-    'SHA256 Test 3': function() {
+    'sha256 mixed input encoding': function() {
       hash = crypto.createHash('sha256');
       hash.update('test1', 'utf8');
       hash.update('fb0a30e26e83b2ac5b9e29', 'hex');
       result = hash.digest('hex');
       expect(result).to.be('52c5a731acf66baf41bdd1cae3260c54566a73635b0559f5395a8ed5556e8e70');
     },
-    'SHA256 Test 4': function() {
-      key = result; //52c5a731acf66baf41bdd1cae3260c54566a73635b0559f5395a8ed5556e8e70
+    'sha256 hmac': function() {
+      key = '52c5a731acf66baf41bdd1cae3260c54566a73635b0559f5395a8ed5556e8e70';
       keyBuffer = new Buffer(key, 'hex');
-
       hash = crypto.createHmac('sha256', keyBuffer);
       hash.update('hello', 'utf8');
       result = hash.digest('hex');
       expect(result).to.be('139f345d99f13d65645709b5d93965ef99a161530361d42a907cfb5332002440');
     },
-    'SHA256 Test 5': function() {
+    'sha256 hmac shorthand': function() {
       result = crypto.hmac('sha256', keyBuffer, 'hello', 'utf8').toString('hex');
       expect(result).to.be('139f345d99f13d65645709b5d93965ef99a161530361d42a907cfb5332002440');
     }
