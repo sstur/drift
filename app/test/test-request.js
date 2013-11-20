ec327d882b84a860e165759daa86c64a8035e659
Simon Sturmer <sstur@me.com>
2013-11-19 21:59:53 -0800

    abstract createMultipartRequest

--- app/test/test-request.js
+++ app/test/test-request.js
@@ -13,9 +13,26 @@ app.on('ready', function(require) {
   var AdapterRequest = require('mock-request');
   var AdapterResponse = require('mock-response');
 
+  app.route('/multipart', function(req, res) {
+    var boundary = 'vXBUZWeMvYUeW9P6lxTi';
+    var data = constructMultipart({
+      boundary: boundary,
+      fields: [{
+        name: 'username',
+        value: 'simo'
+      }],
+      files: [{
+        name: 'myfile.txt',
+        headers: {'Content-Transfer-Encoding': 'base64'},
+        value: '..........'
+      }]
+    });
+    res.end(data.toString('binary'));
+  });
+
   app.addTestSuite('request', {
     'url parsing': function() {
-      var req = getRequest('/go?a=1');
+      var req = createRequest('/go?a=1');
       expect(req.url()).to.be('/go?a=1');
       expect(req.url('raw')).to.be('/go?a=1');
       expect(req.url('rawPath')).to.be('/go');
@@ -24,11 +41,11 @@ app.on('ready', function(require) {
       expect(req.url('qs')).to.be('a=1');
     },
     'qs parsing': function() {
-      var req = getRequest('/go?a=1&B=2');
+      var req = createRequest('/go?a=1&B=2');
       expect(req.query()).to.eql({a: '1', b: '2'});
     },
     'unicode path escaped': function() {
-      var req = getRequest('/f%FCr/f%fcr?a=f%3D%3dr');
+      var req = createRequest('/f%FCr/f%fcr?a=f%3D%3dr');
       expect(req.url('raw')).to.be('/f%FCr/f%fcr?a=f%3D%3dr');
       expect(req.url('rawPath')).to.be('/f%FCr/f%fcr');
       expect(req.url('path')).to.be('/für/für');
@@ -37,7 +54,7 @@ app.on('ready', function(require) {
       expect(req.query('a')).to.be('f==r');
     },
     'unicode path utf8': function() {
-      var req = getRequest('/f%C3%bcr?a=f%C3%bcr');
+      var req = createRequest('/f%C3%bcr?a=f%C3%bcr');
       expect(req.url('raw')).to.be('/f%C3%bcr?a=f%C3%bcr');
       expect(req.url('rawPath')).to.be('/f%C3%bcr');
       expect(req.url('path')).to.be('/für');
@@ -46,26 +63,26 @@ app.on('ready', function(require) {
       expect(req.query('a')).to.be('für');
     },
     'method': function() {
-      var req = getRequest({url: '/'});
+      var req = createRequest({url: '/'});
       expect(req.method()).to.be('GET');
       expect(req.method('GET')).to.be(true);
       expect(req.method('POST')).to.be(false);
-      req = getRequest({method: 'POST', url: '/'});
+      req = createRequest({method: 'POST', url: '/'});
       expect(req.method()).to.be('POST');
-      req = getRequest({method: 'PUT', url: '/'});
+      req = createRequest({method: 'PUT', url: '/'});
       expect(req.method('PUT')).to.be(true);
-      req = getRequest({method: 'delete', url: '/'});
+      req = createRequest({method: 'delete', url: '/'});
       expect(req.method('DELETE')).to.be(true);
-      req = getRequest({url: '/', headers: {'X-HTTP-Method-Override': 'POST'}});
+      req = createRequest({url: '/', headers: {'X-HTTP-Method-Override': 'POST'}});
       expect(req.method()).to.be('POST');
-      req = getRequest({url: '/?_method=head'});
+      req = createRequest({url: '/?_method=head'});
       expect(req.method('head')).to.be(true);
     },
     'headers': function() {
-      var req = getRequest({url: '/', headers: {'User-Agent': 'Mock'}});
+      var req = createRequest({url: '/', headers: {'User-Agent': 'Mock'}});
       expect(req.headers('user-agent')).to.be('Mock');
       expect(req.headers('User-Agent')).to.be('Mock');
-      req = getRequest({
+      req = createRequest({
         url: '/',
         headers: 'User-Agent: Mock\nX-Accel:None\r\nX-Double :a:b: c'
       });
@@ -74,59 +91,60 @@ app.on('ready', function(require) {
       expect(req.headers('X-Double')).to.be('a:b: c');
     },
     'isAjax': function() {
-      var req = getRequest({url: '/', headers: 'X-Requested-With: XMLHttpRequest'});
+      var req = createRequest({url: '/', headers: 'X-Requested-With: XMLHttpRequest'});
       expect(req.headers('x-requested-with')).to.be('XMLHttpRequest');
       expect(req.isAjax()).to.be(true);
     },
     'cookie parsing': function() {
-      var req = getRequest({url: '/', headers: 'Cookie: SID=VGcnZXqyEPtNSWa8Rd7fDyoJxR8OfYm2; EULA=1'});
+      var req = createRequest({url: '/', headers: 'Cookie: SID=VGcnZXqyEPtNSWa8Rd7fDyoJxR8OfYm2; EULA=1'});
       expect(req.cookies('sid')).to.be('VGcnZXqyEPtNSWa8Rd7fDyoJxR8OfYm2');
       expect(req.cookies('EULA')).to.be('1');
     },
     'body parsing': function() {
-      var boundary = 'vXBUZWeMvYUeW9P6lxTi';
       var file = new Buffer('4749463839610100010080FF00C0C0C000000021F90401000000002C00000000010001000002024401003B', 'hex');
-      var data = constructMultipart({
-        boundary: boundary,
+      var req = createMultipartRequest({
         fields: [{
           name: 'username',
           value: 'simo'
         }],
         files: [{
-          name: 'avatar',
+          name: 'image',
           filename: 'image.gif',
           type: 'image/gif',
-          data: file
+          value: file
         }]
-      });
-      var req = getRequest({
-        url: '/',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'multipart/form-data; boundary=' + boundary,
-          'Content-Length': data.length
-        },
-        body: data
       });
       var body = req.body();
       expect(body.username).to.be('simo');
-      expect(body.avatar.type).to.be('file');
-      expect(body.avatar.name).to.be('avatar');
-      expect(body.avatar.fileName).to.be('image.gif');
-      expect(body.avatar.contentType).to.be('image/gif');
-      expect(body.avatar.size).to.be(file.length);
-      expect(body.avatar.hash).to.be(crypto.hash('md5', file).toString('hex'));
+      expect(body.image.type).to.be('file');
+      expect(body.image.name).to.be('image');
+      expect(body.image.fileName).to.be('image.gif');
+      expect(body.image.contentType).to.be('image/gif');
+      expect(body.image.size).to.be(file.length);
+      expect(body.image.hash).to.be(crypto.hash('md5', file).toString('hex'));
     }
   });
 
-  function getRequest(cfg) {
-    var aReq = new AdapterRequest(cfg);
-    var req = new Request(aReq);
-    var aRes = new AdapterResponse();
-    var res = new Response(aRes);
+  function createRequest(cfg) {
+    var req = new Request(new AdapterRequest(cfg));
+    var res = new Response(new AdapterResponse());
     req.res = res;
     res.req = req;
     return req;
+  }
+
+  function createMultipartRequest(cfg) {
+    cfg.boundary = cfg.boundary || 'vXBUZWeMvYUeW9P6lxTi';
+    var data = constructMultipart(cfg);
+    return createRequest({
+      url: '/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data; boundary=' + cfg.boundary,
+        'Content-Length': data.length
+      },
+      body: data
+    });
   }
 
   function constructMultipart(cfg) {
@@ -142,18 +160,24 @@ app.on('ready', function(require) {
     var files = cfg.files || [];
     files.forEach(function(file) {
       body.push('--' + boundary);
-      body.push('Content-Disposition: form-data; name="' + encodeURI(file.name) + '"; filename="' + encodeURI(file.filename) + '"');
+      var name = file.name;
+      var filename = file.filename || name;
+      body.push('Content-Disposition: form-data; name="' + encodeURI(name) + '"; filename="' + encodeURI(filename) + '"');
       var type = file.type || 'application/octet-stream';
       body.push('Content-Type: ' + type);
+      var headers = file.headers || {};
+      forEach(headers, function(name, value) {
+        body.push(name + ': ' + value);
+      });
       body.push('');
-      var data = file.data;
+      var data = file.value;
       if (Buffer.isBuffer(data)) {
         data = data.toString('binary');
       }
       body.push(data);
     });
     body.push('--' + boundary + '--');
-    body = body.join('\r\n')
+    body = body.join('\r\n');
     return new Buffer(body, 'binary');
   }
 
