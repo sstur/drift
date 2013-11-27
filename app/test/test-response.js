bded77b7007861dfbe0a3a3d884c0f9dcd7484a3
Simon Sturmer <sstur@me.com>
2013-11-26 20:12:26 -0800

    begin tests for response

--- app/test/test-response.js
+++ app/test/test-response.js
@@ -14,9 +14,39 @@ app.on('ready', function(require) {
   //blob = new Buffer(blob, 'binary');
 
   app.addTestSuite('response', {
-    'url parsing': function() {
-      var res = createResponse('/go?a=1');
-      expect(res.end()).to.be('');
+    'res.clear()': function(it) {
+      var res = createResponse();
+      res.write('a');
+      res.clear();
+      res.write('b');
+      var result = res.end();
+      expect(result.body).to.be('b');
+    },
+    'res.status()': function(it) {
+    },
+    'res.charset()': function(it) {
+    },
+    'res.headers()': function(it) {
+    },
+    'res.write()': function(it) {
+    },
+    'res.contentType()': function(it) {
+    },
+    'res.cookies()': function(it) {
+    },
+    'res.end()': function(it) {
+    },
+    'res.die()': function(it) {
+    },
+    'res.debug()': function(it) {
+    },
+    'res.getWriteStream()': function(it) {
+    },
+    'res.sendFile()': function(it) {
+    },
+    'res.redirect()': function(it) {
+    },
+    'res.htmlRedirect()': function(it) {
     }
   });
 
@@ -26,6 +56,20 @@ app.on('ready', function(require) {
     var res = new Response(new AdapterResponse());
     req.res = res;
     res.req = req;
+    var _end = res.end;
+    res.end = function() {
+      try {
+        _end.apply(this, arguments);
+      } catch(e) {
+        if (e !== null) throw e;
+      }
+      var result = res._super;
+      return {
+        status: result.status,
+        headers: result.headers,
+        body: result.getBody()
+      }
+    };
     return res;
   }
 
