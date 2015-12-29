217998f18b0bef81c983e6c9635cfec76de1ef26
Simon Sturmer <sstur@me.com>
2015-12-29 09:17:05 +0700

    es6/babel: explicitly specify plugins

--- modules/drift-utils/utils.js
+++ modules/drift-utils/utils.js
@@ -43,12 +43,44 @@ var utils = {
   },
 
   transformES6: function(source) {
+    var plugin = (name) => require.resolve('babel-plugin-' + name);
     var result = babel.transform(source, {
       retainLines: true,
-      presets: [
-        require.resolve('babel-preset-es2015'),
-        require.resolve('babel-preset-stage-2'),
-        require.resolve('babel-preset-react')
+      plugins: [
+        // ecmascript stage-1
+        plugin('transform-class-properties'),
+        // ecmascript stage-2
+        plugin('syntax-trailing-function-commas'),
+        plugin('transform-object-rest-spread'),
+        // ecmascript stage-3
+        plugin('transform-async-to-generator'),
+        plugin('transform-exponentiation-operator'),
+        // ecmascript 2015
+        plugin('check-es2015-constants'),
+        plugin('transform-es2015-arrow-functions'),
+        plugin('transform-es2015-block-scoped-functions'),
+        plugin('transform-es2015-block-scoping'),
+        plugin('transform-es2015-classes'),
+        plugin('transform-es2015-computed-properties'),
+        plugin('transform-es2015-destructuring'),
+        plugin('transform-es2015-for-of'),
+        plugin('transform-es2015-function-name'),
+        plugin('transform-es2015-literals'),
+        plugin('transform-es2015-modules-commonjs'),
+        plugin('transform-es2015-object-super'),
+        plugin('transform-es2015-parameters'),
+        plugin('transform-es2015-shorthand-properties'),
+        plugin('transform-es2015-spread'),
+        plugin('transform-es2015-sticky-regex'),
+        plugin('transform-es2015-template-literals'),
+        plugin('transform-es2015-typeof-symbol'),
+        plugin('transform-es2015-unicode-regex'),
+        plugin('transform-regenerator'),
+        // react/jsx/flow
+        plugin('syntax-flow'),
+        plugin('syntax-jsx'),
+        plugin('transform-flow-strip-types'),
+        plugin('transform-react-jsx')
       ]
     });
     //var {code, map, ast} = result;
