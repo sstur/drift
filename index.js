5077177fa34b3d164a5ffd6042681590d3ac1b3c
Simon <sstur@me.com>
2021-05-18 11:14:11 -0700

    Remove unnecessary IIFE from files

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-(function() {
-  var http = require('http');
+var http = require('http');
 
-  var opts = global.opts || (global.opts = {});
-  opts.fxPath = __dirname;
+var opts = global.opts || (global.opts = {});
+opts.fxPath = __dirname;
 
-  function startServer(opts, callback) {
-    var port = opts.port || 8080;
-    var address = opts.address || '127.0.0.1';
-    var server = http.createServer();
+function startServer(opts, callback) {
+  var port = opts.port || 8080;
+  var address = opts.address || '127.0.0.1';
+  var server = http.createServer();
 
-    server.on('listening', function() {
-      if (callback) callback.apply(this, arguments);
-    });
+  server.on('listening', function() {
+    if (callback) callback.apply(this, arguments);
+  });
 
-    server.on('request', getRequestHandler());
+  server.on('request', getRequestHandler());
 
-    server.on('error', function(e) {
-      if (e.code === 'EADDRINUSE') {
-        port++;
-        server.listen(port, address);
-      } else {
-        throw e;
-      }
-    });
+  server.on('error', function(e) {
+    if (e.code === 'EADDRINUSE') {
+      port++;
+      server.listen(port, address);
+    } else {
+      throw e;
+    }
+  });
 
-    server.listen(port, address);
+  server.listen(port, address);
 
-    return server;
-  }
+  return server;
+}
 
-  function getRequestHandler() {
-    var SyncServer = require('./src/server.js');
-    return SyncServer.requestHandler;
-  }
+function getRequestHandler() {
+  var SyncServer = require('./src/server.js');
+  return SyncServer.requestHandler;
+}
 
-  exports.startServer = startServer;
-  exports.getRequestHandler = getRequestHandler;
-})();
+exports.startServer = startServer;
+exports.getRequestHandler = getRequestHandler;
--- src/package-config.js
+++ src/package-config.js
@@ -1,22 +1,20 @@
-(function() {
-  var fs = require('fs');
-  var join = require('path').join;
+var fs = require('fs');
+var join = require('path').join;
 
-  //the parsed cli arguments from optimist
-  var opts = global.opts || {};
-  //this is the project path; used in patch and app.mappath
-  var basePath = (global.basePath = opts.path || process.cwd());
+//the parsed cli arguments from optimist
+var opts = global.opts || {};
+//this is the project path; used in patch and app.mappath
+var basePath = (global.basePath = opts.path || process.cwd());
 
-  //read one or more config files (if exists) in `path` and aggregate their properties
-  function readJSON(file) {
-    try {
-      var string = fs.readFileSync(join(basePath, file), 'utf8');
-      var data = JSON.parse(string);
-    } catch (e) {
-      return;
-    }
-    return data;
+//read one or more config files (if exists) in `path` and aggregate their properties
+function readJSON(file) {
+  try {
+    var string = fs.readFileSync(join(basePath, file), 'utf8');
+    var data = JSON.parse(string);
+  } catch (e) {
+    return;
   }
+  return data;
+}
 
-  module.exports = readJSON('package.json');
-})();
+module.exports = readJSON('package.json');
