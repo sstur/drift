bdc75d39da6c4ba7fb037798c13302b72c6a1adb
Simon <sstur@me.com>
2021-05-19 07:02:55 -0700

    Remove unused config from src/config/defaults.js

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,16 +1,7 @@
 app.cfg({
   auto_save_uploads: false,
-  virtual_url: false,
   response_404: {
     type: 'text/plain',
     body: '{"error":"404 Not Found"}',
   },
-  smtp: {
-    host: 'localhost',
-    port: 25,
-  },
-  session: {
-    default_datastore: 'memory',
-  },
-  template_defaults: {},
 });
